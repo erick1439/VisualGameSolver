467631796d51f03fcc14d2f1168852d8e3105b04
erick1439 <erick1439@live.com>
2022-02-26 23:31:06 -0500

    word search is pending

--- src/Controllers/GetWordSearchBoardController.js
+++ src/Controllers/GetWordSearchBoardController.js
@@ -1,29 +1,29 @@
-import { set } from 'core-js/core/dict';
-import animals from '../words.json';
-let generator = require('@sbj42/word-search-generator');
+// import { set } from 'core-js/core/dict';
+// import animals from '../words.json';
+// let generator = require('@sbj42/word-search-generator');
 
 const GetWordSearchBoardController = (req, res) => {
 
-    let set = new Set();
-    let temp = new Array();
+    // let set = new Set();
+    // let temp = new Array();
 
-    while (set.size < 10)
-        set.add(animals[Math.floor(Math.random() * animals.length - 1)]);
+    // while (set.size < 10)
+    //     set.add(animals[Math.floor(Math.random() * animals.length - 1)]);
     
-    for (let item of set)
-        temp.push(item.toUpperCase());
+    // for (let item of set)
+    //     temp.push(item.toUpperCase());
 
-    let options = {
-        diagonals: true,
-        width: 15,
-        height: 16,
-        minLength: 3,
-        maxLength: 7,
-        words: temp
-    }
+    // let options = {
+    //     diagonals: true,
+    //     width: 15,
+    //     height: 16,
+    //     minLength: 3,
+    //     maxLength: 7,
+    //     words: temp
+    // }
 
-    let board = generator.generate(options);
-    res.send(board);
+    // let board = generator.generate(options);
+    // res.send(board);
 }
 
 export default GetWordSearchBoardController;
